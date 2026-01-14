cd8479b5821f8c72f8ba06c68f043cf111a088d7
hakyungmin <hkmin3827@gmail.com>
2026-01-14 14:47:46 +0900

    crossorigin 추가

--- src/main/java/com/whatslovermbti/mbti_prj/config/CorsConfig.java
+++ src/main/java/com/whatslovermbti/mbti_prj/config/CorsConfig.java
@@ -6,15 +6,23 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://lovers-mbti.vercel.app"
+        ));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+//        config.setAllowedMethods(List.of(
+//                "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+//        ));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
