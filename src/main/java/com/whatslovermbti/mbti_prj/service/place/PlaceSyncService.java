c26bcaf3ab3453b39ad17b13fdf187692e170adf
hakyungmin <hkmin3827@gmail.com>
2026-03-28 00:50:18 +0900

    도메인 중심 패키지 구조 변경 전

--- src/main/java/com/whatslovermbti/mbti_prj/service/place/PlaceSyncService.java
+++ src/main/java/com/whatslovermbti/mbti_prj/service/place/PlaceSyncService.java
@@ -2,7 +2,6 @@ package com.whatslovermbti.mbti_prj.service.place;
 
 import com.whatslovermbti.mbti_prj.entity.Place;
 import com.whatslovermbti.mbti_prj.infra.kakao.KakaoMapClient;
-import com.whatslovermbti.mbti_prj.repository.PlaceRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class PlaceSyncService {
 
-    private final PlaceRepository placeRepository;
     private final KakaoMapClient kakaoMapClient;
 
     public void validatePlace(Place place) {
